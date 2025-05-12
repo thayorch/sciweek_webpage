5bd51ce5fdd5b260d7006bff1fc61fc2442e1662
THAYORZZ\thatc <thatcharnon.10210@gmail.com>
2025-05-13 02:15:35 +0700

    version 1.0

---
+++ app/components/Incorrect.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import { ref, computed } from 'vue'
+import { defineProps } from 'vue'
+
+const props = defineProps<{
+  quizIndex: number
+}>()
+
+const answerSource = ref<string[]>([
+  "quiz1/ans/1.jpg",
+  "quiz1/ans/2.jpg",
+  "quiz1/ans/3.jpg",
+  "quiz1/ans/4.jpg",
+  "quiz1/ans/5.jpg",
+])
+
+const answerImage = computed(() => answerSource.value[props.quizIndex])
+</script>
+
+<template>
+  <v-container class="pa-md-12 text-center">
+    <v-alert
+      class="mx-auto mb-4"
+      icon="$error"
+      color="error"
+      max-width="700"
+      rounded="lg"
+      title="Incorrect"
+    />
+    <v-img :src="answerImage" max-width="300" class="mx-auto" />
+  </v-container>
+</template>
--- app/pages/quiz.vue
+++ app/pages/quiz.vue
@@ -1,87 +1,120 @@
 <template>
+  <v-dialog
+    v-model="alertCorrect"
+    scrollable
+    persistent
+    :overlay="false"
+    max-width="500px"
+    transition="dialog-transition"
+    @click="alertCorrect = !alertCorrect"
+    class="animate__animated animate__fadeIn animate__shakeY mx-auto"
+  >
+    <Correct :quizIndex="quizIndex" />
+  </v-dialog>
+
+  <v-dialog
+    v-model="alertIncorrect"
+    scrollable
+    persistent
+    :overlay="false"
+    max-width="500px"
+    transition="dialog-transition"
+    @click="alertIncorrect = !alertIncorrect"
+     class="animate__animated animate__fadeIn animate__shakeY mx-auto"
+  >
+    <Incorrect :quizIndex="quizIndex" />
+  </v-dialog>
+
+  <v-btn color="success" @click="alertCorrect = !alertCorrect">check</v-btn>
+  <v-btn color="error" @click="alertIncorrect = !alertIncorrect">check</v-btn>
+
   <v-container class="animate__animated animate__fadeIn animate__shakeY">
-    <h1 class="text-center text-white">Quiz {{ quiz_count }}</h1>
-    <div class="ma-8 gamepanel mx-auto text-center">
-      
+    <h1 class="text-center text-white">Quiz {{ quizIndex + 1 }} / 5</h1>
+
+    <div class="mt-8 gamepanel mx-auto text-center">
       <h1 class="text-center text-white">Guess the shadow</h1>
-      <div class="picture">
-        <img src="~/assets/quiz1/conan.jpg" height="200" alt="">
-      </div>
-      <div class="answer" >
-        <v-row no-gutters>
-          <v-col class="pa-2 ma-2">
-            <v-card
-              class="mx-auto"
-              width="200"
-              height="55"
-              subtitle=""
-              title="Answer 1"
-              link
-            ></v-card>
-          </v-col>
+      <v-img
+        :src="imgSource[quizIndex]"
+        class="ma-2"
+        height="250"
+        alt="images"
+      />
 
-          <v-col class="pa-2 ma-2">
+      <div class="option ma-8">
+        <v-row>
+          <v-col
+            v-for="(data, index) in optionData"
+            :key="index"
+            cols="6"
+            class="pa-2"
+          >
             <v-card
               class="mx-auto"
-              width="200"
+              width="100%"
               height="55"
-              title="Answer 2"
-              link
-            ></v-card>
-          </v-col>
-
-          <v-col class="pa-2 ma-2">
-            <v-card
-              class="mx-auto"
-              width="200"
-              height="55"
-              title="Answer 3"
-              link
-            ></v-card>
-          </v-col>
-
-          <v-col class="pa-2 ma-2">
-            <v-card
-              class="mx-auto"
-              width="200"
-              height="55"
-              title="Answer 4"
-              link
+              :title="data.title"
+              @click="checkAnswer(data.id)"
             ></v-card>
           </v-col>
         </v-row>
       </div>
-      <!-- <v-btn
-        v-if="!alert_status"
-        class="ma-10 border-opacity-0"
-        rounded
-        color="white"
-        @click="updateCount"
-      >
-        {{ btn_option }}
-      </v-btn> -->
     </div>
   </v-container>
 </template>
 
 <script setup lang="ts">
-const alert_status = ref(false);
-const quiz_count = ref<number>(1);
-const btn_option = ref<string>("Next");
+const router = useRouter();
+const optionData = ref([
+  { title: "Answer 1", id: 1 },
+  { title: "Answer 2", id: 2 },
+  { title: "Answer 3", id: 3 },
+  { title: "Answer 4", id: 4 },
+]);
 
-const updateCount = () => {
-  quiz_count.value++;
-  if (quiz_count.value === 10) {
-    alert_status.value = !alert_status.value;
+const imgSource = ref<string[]>([
+  "quiz1/1.jpg",
+  "quiz1/2.jpg",
+  "quiz1/3.jpg",
+  "quiz1/4.jpg",
+  "quiz1/5.jpg",
+]);
+
+const alertCorrect = ref(false);
+const alertIncorrect = ref(false);
+const quizIndex = ref(0);
+const answerList = [1, 4, 2, 4, 3];
+
+const checkAnswer = (answer: number) => {
+  if (answer === answerList[quizIndex.value]) {
+    alertCorrect.value = true;
+    setTimeout(() => {
+      alertCorrect.value = false;
+    }, 1000);
+  } else {
+    alertIncorrect.value = true;
+    setTimeout(() => {
+      alertIncorrect.value = false;
+    }, 1000);
+  }
+
+  updateIndex();
+};
+
+const updateIndex = () => {
+  if (quizIndex.value >= 4) {
+    setTimeout(() => {
+      router.push("/thanks");
+    }, 1500);
+  } else {
+    quizIndex.value++;
   }
 };
 </script>
 
 <style lang="scss">
 .gamepanel {
-  width: 50vw;
-  height: 70vh;
+  height: 80vh;
+  border-radius: 5px;
   background: #ffffff77;
 }
-
 </style>
