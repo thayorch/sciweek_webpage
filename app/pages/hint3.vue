8527b53c7f8a7b9016342055dfb42bf9e3a75a98
THAYORZZ\thatc <thatcharnon.10210@gmail.com>
2025-05-13 23:56:49 +0700

    version 1.13

--- app/pages/hint3.vue
+++ app/pages/hint3.vue
@@ -4,24 +4,26 @@
   >
     <div class="mt-16 pa-16"></div>
     <h1 class="text-center text-white">! Thanks For Playing !</h1>
-    <h2 class="text-center text-white">The Ways Hint for you</h2>
-    <h1 class="text-center text-white"> 
-      <v-img > </v-img>
-    </h1>
-    <p class="text-center text-white">You can go to the next ways</p>
+    <h2 class="text-center text-white">You got the Goblet of Fire</h2>
 
+    <div class="text-center">
+      <v-img
+        :src="img_source"
+        height="200"
+        width="200"
+        class="text-center mx-auto"
+      ></v-img>
+    </div>
 
+    <p class="text-center text-white">You can go to the entrance</p>
     <div class="text-center">
       <v-btn class="ma-10 border-opacity-0" rounded color="white" to="/"
         >Dashbord</v-btn
       >
     </div>
-    <div class="text-center">
-    </div>
   </v-container>
 </template>
 
-
-<script>
-
+<script setup>
+const img_source = "vials/goblet.png";
 </script>
